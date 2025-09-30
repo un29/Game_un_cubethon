1dd92b835908dea84465b36694af961a67e56e19
un_29 <emily6370@gmail.com>
2025-09-30 10:36:07 +0800

    Update FollowPlayer.cs

--- Assets/Script/FollowPlayer.cs
+++ Assets/Script/FollowPlayer.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+// 相機跟隨角色
     public Transform player;
-    public Vector3 offset;
+    public Vector3 offset;    // 相機與玩家之間的距離
 
     void Update(){
-
+        // 相機每幀更新位置
         transform.position = player.position + offset;
     }
 }
